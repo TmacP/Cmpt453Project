79e25d68188c2261a1e43300004705b595ea807d
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2024-11-03 16:14:13 -0600

    fix well done gui node

--- main/level.go
+++ main/level.go
@@ -1,33 +1,14 @@
 components {
   id: "level"
   component: "/main/level.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
+}
+components {
+  id: "gui"
+  component: "/main/level.gui"
 }
 embedded_components {
   id: "brickfactory"
   type: "collectionfactory"
   data: "prototype: \"/main/brick/brick.collection\"\n"
-  "load_dynamically: false\n"
   ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
 }
