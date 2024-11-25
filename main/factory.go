3dc3ed121346e3494832bcc8f9ec52f4e1e8edc9
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2024-11-25 16:23:54 -0600

    update and it works

--- main/factory.go
+++ main/factory.go
@@ -40,3 +40,19 @@ embedded_components {
     y: 300.0
   }
 }
+embedded_components {
+  id: "target"
+  type: "label"
+  data: "size {\n"
+  "  x: 128.0\n"
+  "  y: 32.0\n"
+  "}\n"
+  "text: \"Label\"\n"
+  "font: \"/builtins/fonts/default.font\"\n"
+  "material: \"/builtins/fonts/label-df.material\"\n"
+  ""
+  position {
+    x: 180.0
+    y: 280.0
+  }
+}
