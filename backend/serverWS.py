59524d8e6d8c4a380aca3587a314215d6bc65048
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2024-11-25 11:31:34 -0600

    test on fly deploy

--- backend/serverWS.py
+++ backend/serverWS.py
@@ -12,7 +12,7 @@ async def echo(websocket):
 
 
 async def main():
-    async with serve(echo, "localhost", 8765) as server:
+    async with serve(echo, "0.0.0.0", 12345) as server:
         await server.serve_forever()
 
 
