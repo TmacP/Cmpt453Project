7ef9290531d7156fff33cd0127736e47a3f7b3fa
Tristen MacPherson (trm003) <tristen.macpherson@usask.ca>
2024-12-14 20:11:04 -0600

    clean up dir structure and todo

--- main/koi.go
+++ main/koi.go
@@ -2,7 +2,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "default_animation: \"anim\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
+  "material: \"/main/koistyle/koistyle.material\"\n"
   "textures {\n"
   "  sampler: \"texture_sampler\"\n"
   "  texture: \"/main/koi.tilesource\"\n"
